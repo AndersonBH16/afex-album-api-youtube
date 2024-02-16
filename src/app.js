144ad73d1b2f6621d5c8beaa7be83925bd85c381
Anderson Blas <andi_1516@hotmail.com>
2024-02-16 10:39:58 -0500

    Configurar CORS para comunicación entre dominios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import morgan from 'morgan'; // para revisar en consola las solicitudes http (opcional)
+import cors from 'cors';
+import videoRoutes from './routes/video.routes.js';
 
 const app = express();
+
+app.use(cors()); // permite que todos los dominios se comuniquen en el mismo servidor
 app.use(morgan('dev'));
+app.use(express.json()); // middleware que permite leer request.body a formato js
+
+app.use(videoRoutes);
 
 export default app;
