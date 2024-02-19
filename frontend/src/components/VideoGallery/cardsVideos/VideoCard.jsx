796a3f3ab67c7b4c1329cfecfd04e8285c15db08
Anderson Blas <andi_1516@hotmail.com>
2024-02-18 21:51:41 -0500

    Deshabilitar comportamiento del div button al momento de hacer click en el boton de eliminar video

--- frontend/src/components/VideoGallery/cardsVideos/VideoCard.jsx
+++ frontend/src/components/VideoGallery/cardsVideos/VideoCard.jsx
@@ -5,12 +5,14 @@ import { useState } from "react";
 
 export function VideoCard({ video }){
     const [showModal, setShowModal] = useState(false);
+    const handleDeleteButtonClick = (e) => {        
+        
+        e.stopPropagation();
+    };
 
-    return (<>
-        <button className="video-card" type="button" onClick={() => setShowModal(true)}>
-            <div className="video-delete"
-                data-id-video = {video.videoId}
-            >                
+    return (<>        
+        <div className="video-card" type="button" onClick={() => setShowModal(true)}>
+            <button className="video-delete" onClick={ handleDeleteButtonClick }>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="12"
@@ -21,10 +23,10 @@ export function VideoCard({ video }){
                 >
                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
                 </svg>
-            </div>
+            </button>
             <img className="video" src={video.thumbnailUrl} alt={video.title} />
             <div className="video-duration">{video.duration}</div>
-        </button>
+        </div>
         <ModalVideo videoInfo={video} showModal={showModal} closeModal={() => setShowModal(false)} />
     </>
     );
